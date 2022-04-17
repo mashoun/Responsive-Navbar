558444fa12d350eb5fac19f55dbb493cb1aa8422
mashoun <mashoun.me@gmail.com>
2022-04-18 00:18:13 +0300

    re write responsive

---
+++ public/app.js
@@ -0,0 +1,17 @@
+function activate() {
+    // Get the container element
+    var btnContainer = document.querySelectorAll('ul')[0]
+
+    // Get all buttons with class="btn" inside the container
+    var btns = btnContainer.querySelectorAll('li')
+
+    // Loop through the buttons and add the active class to the current/clicked button
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function () {
+            var current = document.getElementsByClassName("active");
+            current[0].className = current[0].className.replace(" active", "");
+            this.className += " active";
+        });
+    }
+}
+activate()
